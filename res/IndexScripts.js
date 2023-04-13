95f9a1ef0ae30d79060ff341ea7d41e1e065d741
G-xming <120670354+G-xming@users.noreply.github.com>
2023-04-13 13:20:01 -0500

    Create IndexScripts.js

---
+++ res/IndexScripts.js
@@ -0,0 +1,11 @@
+function IB(){
+    document.body.innerHTML = '<iframe src="https://raw.githack.com/G-xming/Play/main/IB/index.html" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>'
+}
+
+function AS(){
+    location.href= 'antisecurly.html'
+}
+
+function SNPB(){
+    location.href= 'securlynewpointblank.html'
+}
